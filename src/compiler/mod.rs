eb0bfbcc38a8790d5d24464d2bb9e4f2eb8640d3
ASTRALLIBERTAD <astrallibertad@gmail.com>
2025-11-03 14:34:30 +0800

    Remove some files that are not needed

--- src/compiler/mod.rs
+++ src/compiler/mod.rs
@@ -3,23 +3,10 @@ pub mod parser;
 pub mod ast;
 pub mod interpreter;
 pub mod semantic;
-// pub mod llvm;
-pub mod llvm_android;
 pub mod value;
-pub mod android_compiler;
 pub mod builtins;
 
 pub use ast::*;
 pub use lexer::Lexer;
 pub use parser::Parser;
 pub use interpreter::Interpreter;
-
-// // Android compilation functions
-// pub use codegen_inkwell::{
-//     InkwellAndroidCodegen,
-//     generate_llvm_android_inkwell,
-//     compile_to_llvm_android_inkwell,
-//     compile_to_android_object,
-// };
-
-// pub use android_compiler::AndroidCompiler;
--- target/debug/deps/astrallang.d
+++ target/debug/deps/astrallang.d
@@ -1,6 +1,6 @@
-C:\Users\Boss\Documents\astrallang\astrallang\target\debug\deps\astrallang.d: src\main.rs src\compiler\mod.rs src\compiler\lexer.rs src\compiler\parser.rs src\compiler\ast.rs src\compiler\interpreter.rs src\compiler\semantic.rs src\compiler\llvm_android.rs src\compiler\value.rs src\compiler\android_compiler.rs src\compiler\builtins.rs
+C:\Users\Boss\Documents\astrallang\astrallang\target\debug\deps\astrallang.d: src\main.rs src\compiler\mod.rs src\compiler\lexer.rs src\compiler\parser.rs src\compiler\ast.rs src\compiler\interpreter.rs src\compiler\semantic.rs src\compiler\value.rs src\compiler\builtins.rs
 
-C:\Users\Boss\Documents\astrallang\astrallang\target\debug\deps\astrallang.exe: src\main.rs src\compiler\mod.rs src\compiler\lexer.rs src\compiler\parser.rs src\compiler\ast.rs src\compiler\interpreter.rs src\compiler\semantic.rs src\compiler\llvm_android.rs src\compiler\value.rs src\compiler\android_compiler.rs src\compiler\builtins.rs
+C:\Users\Boss\Documents\astrallang\astrallang\target\debug\deps\astrallang.exe: src\main.rs src\compiler\mod.rs src\compiler\lexer.rs src\compiler\parser.rs src\compiler\ast.rs src\compiler\interpreter.rs src\compiler\semantic.rs src\compiler\value.rs src\compiler\builtins.rs
 
 src\main.rs:
 src\compiler\mod.rs:
@@ -9,7 +9,5 @@ src\compiler\parser.rs:
 src\compiler\ast.rs:
 src\compiler\interpreter.rs:
 src\compiler\semantic.rs:
-src\compiler\llvm_android.rs:
 src\compiler\value.rs:
-src\compiler\android_compiler.rs:
 src\compiler\builtins.rs:
